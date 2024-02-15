6b37cd354382b291483965ab5ba39e1f190cecf9
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2024-02-16 00:49:06 +0500

    created DB connection with app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const xss = require('xss');
+const helmet = require('helmet');
+const cors = require('cors');
+const compression = require('compression');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+// Database connection
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
+// app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/policies', require('./routes/policyRoutes'));
+// Add other routes...
 
-//Add the code for execution
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
