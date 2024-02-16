a71ec249f3819b9a8f1fa11ca64cf659453c7a83
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2024-02-16 10:54:40 +0500

    Added Middleware and refactored app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const compression = require('compression');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const limiter = require('./middleware/rateLimit');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Database connection
-connectDB();
-
 // Middlewares
+app.use(limiter);
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
+// Database connection
+connectDB();
+
 // Routes
-// app.use('/api/users', require('./routes/userRoutes'));
-// app.use('/api/policies', require('./routes/policyRoutes'));
-// Add other routes...
+app.use("/", require("./routes/indexRoute"));
 
 // Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
