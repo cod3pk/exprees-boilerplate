c2f0229bb4cf364cca5b201b7ec05e384eac4a90
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2024-02-20 18:13:11 +0500

    Refactoring structure

--- src/controllers/auth/registerController.js
+++ src/controllers/auth/registerController.js
@@ -28,9 +28,16 @@ exports.registerUser = async (req, res) => {
         await newUser.save();
 
         // Respond with success (could also generate and return a JWT token here)
-        res.status(201).json({message: 'User registered successfully.'});
+        res.status(201).json({
+            message: 'User registered successfully.',
+            user_id: newUser._id
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({message: 'Error registering user', error: error.message});
     }
 };
+
+exports.updateUserProfile = async (req, res) => {
+
+}
