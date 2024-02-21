09e4cff320238229eb0283bf15d8b91571c63cdd
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2024-02-21 16:45:03 +0500

    Implemented JWT Auth and Protected Routes

--- src/controllers/auth/loginController.js
+++ src/controllers/auth/loginController.js
@@ -1,38 +1,42 @@
 const bcrypt = require('bcrypt');
 const userModel = require('../../models/User');
+const jwt = require('jsonwebtoken');
 
 exports.login = async (req, res) => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email});
 
-        // Find the user by email
-        const user = await userModel.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        // Compare the submitted password with the hashed password in the database
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-
-        // At this point, the user has been authenticated
-        // You can generate a token or handle the login response as needed
-        // For demonstration, let's just return the user's data without sensitive information
-        const { password: _, ...userDataWithoutPassword } = user.toObject();
-
-        res.status(200).json({
-            message: 'Login successful',
-            data: userDataWithoutPassword
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({
-            message: 'Unable to login at the moment',
-            error: err.message
-        });
+    if (!user) {
+      return res.status(404).json({message: 'User not found'});
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({message: 'Invalid credentials'});
+    }
+
+    const token = jwt.sign(
+      {userId: user._id, email: user.email},
+      process.env.JWT_SECRET, {
+        expiresIn: '24h'
+      }
+    );
+
+    const {password: _, __v: __, ...filteredUserData} = user.toObject();
+
+    res.status(200).json({
+      message: 'Login successful',
+      token: token,
+      data: filteredUserData
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'Unable to login at the moment',
+      error: err.message
+    });
+  }
 };
--- src/controllers/auth/registerController.js
+++ src/controllers/auth/registerController.js
@@ -1,70 +1,76 @@
 const bcrypt = require('bcrypt');
 const userModel = require('../../models/User');
+const jwt = require('jsonwebtoken');
 
 // Register User
 exports.registerUser = async (req, res) => {
 
-    const {firstName, lastName, email, password, role = 'customer'} = req.body;
+  const {firstName, lastName, email, password, role = 'customer'} = req.body;
 
-    try {
-        const existingUser = await userModel.findOne({email});
-        if (existingUser) {
-            return res.status(400).json({message: 'User already exists with this email.'});
-        }
-
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        // Create new user
-        const newUser = new userModel({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-            role,
-        });
-
-        await newUser.save();
-
-        // Respond with success (could also generate and return a JWT token here)
-        res.status(201).json({
-            message: 'User registered successfully.',
-            user_id: newUser._id
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: 'Error registering user', error: error.message});
+  try {
+    const existingUser = await userModel.findOne({email});
+    if (existingUser) {
+      return res.status(400).json({message: 'User already exists with this email.'});
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new userModel({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newUser.save();
+
+    const token = jwt.sign(
+      {userId: newUser._id, email: newUser.email},
+      process.env.JWT_SECRET,
+      {expiresIn: '24h'}
+    );
+
+    res.status(201).json({
+      message: 'User registered successfully.',
+      token: token,
+      user_id: newUser._id
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: 'Error registering user', error: error.message});
+  }
 };
 
 // Update User profile
 exports.updateUserProfile = async (req, res) => {
-    const userID = req.params.userId
-    const {phone, cnic, dob, address, profileImage} = req.body;
+  const userID = req.params.userId
+  const {phone, cnic, dob, address, profileImage} = req.body;
 
-    try {
-        const updatedUser = await userModel.findByIdAndUpdate(
-            userID,
-            {phone, cnic, dob, address, profileImage},
-            {new: true, runValidators: true}
-        )
+  try {
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userID,
+      {phone, cnic, dob, address, profileImage},
+      {new: true, runValidators: true}
+    )
 
-        if (!updatedUser) {
-            return res.status(404).json({
-                message: 'User Not found'
-            });
-        }
-
-        res.status(200).json({
-            message: 'Profile has been updated successfully',
-            data: updatedUser
-        });
-
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({
-            message: 'Error updating the profile', error: err.message
-        })
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: 'User Not found'
+      });
     }
+
+    const {password: _, __v: __, ...userFilteredData} = updatedUser.toObject();
+
+    res.status(200).json({
+      message: 'Profile has been updated successfully',
+      data: userFilteredData
+    });
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      message: 'Error updating the profile', error: err.message
+    })
+  }
 
 }
